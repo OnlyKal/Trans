ba7c65ddab483b9e934e3d8ae915601f29caba55
onlykal <onlykal64@gmail.com>
2022-01-14 09:24:23 +0100

    project initialieze

---
+++ index.php
@@ -0,0 +1,10 @@
+
+    <link rel="stylesheet" href="style/style.css">
+    <link href="style/tailwind.min.css" rel="stylesheet"/>
+
+
+    <div id="main" class="flex justify-center items-center h-full">
+            <div id="login" class="h-full p-12">
+                    <label for="">Login Page</label>
+            </div>
+    </div>
