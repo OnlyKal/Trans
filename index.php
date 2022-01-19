80d9789d1d5346f0b43c0d0d80adceb238ccc52f
onlykal <onlykal64@gmail.com>
2022-01-19 08:02:43 +0100

    kal

--- index.php
+++ index.php
@@ -4,8 +4,13 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet"/>
     <script defer src="https://unpkg.com/alpinejs@3.2.3/dist/cdn.min.js"></script>
 
-    <div id="main" class="flex justify-center items-center h-full py-12 bg-gray-100">
-            <div id="login" class="flex flex-col h-full p-6 shadow-xl  bg-white rounded-xl items-center">                    
+    <div id="main" class="flex justify-center  h-full bg-gray-200 shadow-xl">
+                       <div  id="menu" class="bg-white w-full flex h-12 items-center justify-center">
+                              <div class="bg-blue-400 hover:bg-blue-500 text-white py-1 px-5 flex justify-center items-center text-sm rounded mx-3 cursor-pointer">New user</div>     
+                       </div>
+
+
+                        <!-- <div id="login" class="flex flex-col h-full p-6 shadow-xl  bg-white rounded-xl items-center">                    
                             <img src="images/log.png" alt="" class="w-36 m-6">
                             <div class="flex">
                                 <label for="" class="font-bold text-xl text-blue-500">Financial </label>
@@ -21,8 +26,11 @@
                                 <input type="password" id="password" name="password" class="w-full h-full rounded-md px-2  outline-none text-sm" placeholder="User mail | Phone number">
                             </div>                
                             
-                            <div class="bg-blue-400 w-full flex items-center justify-center p-2 mt-3 hover:bg-blue-500 cursor-pointer font-bold text-white rounded">
+                            <div class="bg-blue-400 w-full flex items-center justify-center p-2 mt-5 hover:bg-blue-500 cursor-pointer font-bold text-white rounded">
                                 Connexion
-                            </div>
-            </div>
+                            </div>                           
+                        </div>
+                        <div id="add-user" class="">
+                              
+                        </div> -->
     </div>
